b472cbe671e68e5b7695f9b41f3db29daa0caa01
Jilha jos <93869128+Jilhajos@users.noreply.github.com>
2023-05-14 18:20:29 +0530

    Update script1.js

--- module 4 solution/script1.js
+++ module 4 solution/script1.js
@@ -1,5 +1,5 @@
 (function(){
-	var names=["jilha","shiji","jose","jismi","chacko","cris"];
+	var names=["Yaakov","john","jen","jason","paul","frank","larry","paula","laura","jim"];
 	for(var i=0;i<names.length;i++){
 		var firstletter=names[i].charAt(0).toLowerCase();
 		if(firstletter==='j'){
